a83f751667acbf4f9d2a384c62134378c04ff01b
printf-jin <dlghwls7662@naver.com>
2022-06-15 01:20:14 +0900

    Item 재고관련 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exeption.NotEnoughStockExeption;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,4 +26,23 @@ public abstract class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    //재고 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    //재고 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockExeption("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
